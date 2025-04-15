1923bf2061f77d3b2b1616c5be5b527b4e7cc666
Your Name <you@example.com>
2025-04-15 16:33:33 +0900

    포인트 구현체 작성

--- src/main/java/kr/hhplus/be/server/infrastructure/PointRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/PointRepository.java
@@ -1,11 +1,11 @@
 package kr.hhplus.be.server.infrastructure;
 
 import kr.hhplus.be.server.domain.PointEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface  PointRepository extends JpaRepository<PointEntity, String> {
+
+public interface  PointRepository {
     //사용자 아이디로 사용자의 포인트 리스트 조회
     PointEntity findByUserId(String userId);
+    PointEntity save(PointEntity point);
 }
---
+++ src/main/java/kr/hhplus/be/server/infrastructure/PointRepositoryImpl.java
@@ -0,0 +1,23 @@
+package kr.hhplus.be.server.infrastructure;
+
+import kr.hhplus.be.server.domain.PointEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PointRepositoryImpl implements PointRepository{
+    private final JpaPointRepository jpaPointRepository;
+
+    public PointRepositoryImpl(JpaPointRepository jpaPointRepository) {
+        this.jpaPointRepository = jpaPointRepository;
+    }
+
+    @Override
+    public PointEntity findByUserId(String userId) {
+        return jpaPointRepository.findByUserId(userId);
+    }
+
+    @Override
+    public PointEntity save(PointEntity point) {
+        return jpaPointRepository.save(point);
+    }
+}
