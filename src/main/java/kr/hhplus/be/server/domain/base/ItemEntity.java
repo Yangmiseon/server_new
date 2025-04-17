a904c24acb7f251d8e0b54c8c77e531029662da9
hanghae99 <didaltjs87@gmail.com>
2025-04-18 01:26:33 +0900

    유저,상품 기능

--- src/main/java/kr/hhplus/be/server/domain/base/ItemEntity.java
+++ src/main/java/kr/hhplus/be/server/domain/base/ItemEntity.java
@@ -7,6 +7,10 @@ import lombok.Setter;
 import lombok.ToString;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
 @Getter
 @Setter
 @ToString
@@ -18,6 +22,14 @@ public class ItemEntity {
     @Id
     @Column(nullable = false)
     String itemId;
+    //ID생성
+    public void generateId() {
+        if (itemId == null) {
+            String yearMonth = LocalDate.now().format(DateTimeFormatter.ofPattern("yyMM"));
+            String uuidPart = UUID.randomUUID().toString().replace("-", "").substring(0, 8);
+            this.itemId = "item" + yearMonth + "-" + uuidPart;
+        }
+    }
 
     @Column(nullable = false)
     String itemName;
--- src/main/java/kr/hhplus/be/server/infrastructure/base/ItemRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/base/ItemRepository.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<ItemEntity, String> {
-    List<ItemEntity> findByItemId(String itemId);
+    ItemEntity findByItemId(String itemId);
+    ItemEntity findByItemIdAndItemQuantity(String itemId, int quantity);
 
 }
 
